1fdfd5852387d46b76d8e0f709e25b848797e12b
Alexander Tuxen <tuxen@silpion.de>
2021-08-30 15:00:38 +0200

    simplified mister_get.sh

--- mister_get.sh
+++ mister_get.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-scp root@mister:/media/fat/saves/SNES/Earthbound.sav /tmp/ &&\
-cp /tmp/Earthbound.sav . &&\
-cp /tmp/Earthbound.sav Earthbound\ \(USA\).srm &&\
+scp root@mister:/media/fat/saves/SNES/EarthBound.sav Earthbound\ \(USA\).srm &&\
 echo "Fetched save file from MiSTer"
 
